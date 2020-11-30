bbab618016ccf8b83f42c90a3ca2211286c534e4
Hidenari Kobayashi <hide1982aug@gmail.com>
2020-11-30 20:22:20 +0900

    feat: create stub

---
+++ src/stub/index.ts
@@ -0,0 +1,60 @@
+import { ResponseOxfordDictionary } from "types"
+
+export const dictionaryStub: ResponseOxfordDictionary = {
+  metadata: "metadata",
+  query: "query",
+  results: [
+    {
+      id: "id",
+      language: "language",
+      type: "type",
+      word: "word",
+      pronunciations: [
+        {
+          dialects: ["dilect"],
+          audioFile: "audioFile",
+          phoneticNotation: "IPA",
+          phoneticSpelling: "phoneticSpelling",
+        },
+      ],
+      lexicalEntries: [
+        {
+          language: "en-us",
+          lexicalCategory: { id: "id", text: "verb" },
+          text: "text",
+          entries: [
+            {
+              etymologies: ["etymologies"],
+              inflections: [{ inflectedForm: "inflectedForm" }],
+              senses: [
+                {
+                  id: "id",
+                  shortDefinitions: ["shortDefinitions"],
+                  examples: [
+                    {
+                      definitions: ["definitions"],
+                      domains: [{ id: "id", text: "text" }],
+                      notes: [
+                        {
+                          domains: [{ id: "id", text: "test" }],
+                          id: "id",
+                          language: "en-us",
+                          regions: [{ id: "id", text: "text" }],
+                          registers: [{ id: "id", text: "text" }],
+                          text: "text",
+                        },
+                      ],
+                      registers: [{ id: "id", text: "text" }],
+                      senseIds: ["id"],
+                      text: "text",
+                    },
+                  ],
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+  ],
+}
