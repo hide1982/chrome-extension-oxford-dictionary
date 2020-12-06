e3a2d8c6c0c92980f7a4444d9585c252ad115f2b
Hidenari Kobayashi <hide1982aug@gmail.com>
2020-12-06 18:24:55 +0900

    feat: create Dictionary component

---
+++ src/components/Dictionary.tsx
@@ -0,0 +1,44 @@
+import React from "react"
+import styled from "styled-components"
+import { border, boxShadow } from "styles"
+
+import { Pronunciation } from "types"
+import { ensure } from "utils"
+import { dictionaryStub } from "stub"
+
+import ControlBar from "components/ControlBar"
+import WordHeading from "components/WordHeading"
+import WordContent from "components/WordContent"
+
+const Container = styled.div`
+  ${boxShadow.default};
+  ${border.default};
+  width: 500px;
+  min-height: 300px;
+  background-color: ${({ theme }) => theme.backgroundColor.main};
+  padding: 12px;
+`
+
+const getIpaPhonetiation = (pronunciations: Pronunciation[]) =>
+  pronunciations.find(
+    (pronunciation): pronunciation is Required<Pronunciation> =>
+      pronunciation.phoneticNotation === "IPA"
+  )
+
+const Dictionary: React.FC = () => {
+  const result = dictionaryStub.results[0]
+  const { word, pronunciations, lexicalEntries } = result
+  const { audioFile, phoneticSpelling } = ensure(
+    getIpaPhonetiation(pronunciations)
+  )
+
+  return (
+    <Container>
+      <ControlBar />
+      <WordHeading values={{ word, audioFile, phoneticSpelling }} />
+      <WordContent lexicalEntries={lexicalEntries} />
+    </Container>
+  )
+}
+
+export default Dictionary
