3130ab1ed3c9b6bdd40730ada88618bb80d70c1d
Hidenari Kobayashi <hide1982aug@gmail.com>
2021-01-17 16:47:13 +0900

    feat: add DictionaryLang enum

--- src/constant/index.ts
+++ src/constant/index.ts
@@ -27,6 +27,15 @@ export const enum SearchType {
 
 export const DARK_MODE_MEDIA_QUERY = "(prefers-color-scheme: dark)"
 
-export enum DictionaryLang {
-  EN = "en-us",
+export const enum DictionaryLang {
+  English_US = "en-us",
+  English_GB = "en-gb",
+  Spanish = "es",
+  French = "fr",
+  Gujarati = "gu",
+  Hindi = "hi",
+  Latvian = "lv",
+  Romanian = "ro",
+  Swahili = "sw",
+  Tamil = "ta",
 }
