1bf0f4c95986fcc6102126a4b770a0781fe4cd81
Hidenari Kobayashi <hide1982aug@gmail.com>
2021-01-20 01:44:56 +0900

    feat: add background.ts

--- src/background.ts
+++ src/background.ts
@@ -1,2 +1,30 @@
 import _ from "lodash"
-console.log("hoge")
+import { ResponseOxfordDictionary } from "types"
+import {
+  OXFORD_DICTIONARY_API_BASE_URL,
+  OXFORD_DICTIONARY_APP_ID,
+  OXFORD_DICTIONARY_APP_KEY,
+} from "./config"
+import { DictionaryLang } from "./constant"
+
+const fetchWards = async (
+  word: string,
+  lang: DictionaryLang = DictionaryLang.English_US
+): Promise<ResponseOxfordDictionary> => {
+  const url = `${OXFORD_DICTIONARY_API_BASE_URL}/words/${lang}?q=${word}`
+  const response = await fetch(url, {
+    credentials: "include",
+    headers: {
+      app_id: OXFORD_DICTIONARY_APP_ID,
+      app_key: OXFORD_DICTIONARY_APP_KEY,
+    },
+  })
+  return response.json()
+}
+
+chrome.runtime.onMessage.addListener(
+  (message: string, sender, sendResponse) => {
+    fetchWards(message).then(sendResponse)
+    return true
+  }
+)
