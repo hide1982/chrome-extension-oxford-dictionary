75a39d9dd6d21b78efc153cd91d73b4395d57f9b
Hidenari Kobayashi <hide1982aug@gmail.com>
2021-01-20 20:42:40 +0900

    chore: update sample.config.ts

--- src/sample.config.ts
+++ src/sample.config.ts
@@ -1,3 +1,4 @@
-export const OXFORD_DICTIONARY_API_BASE_URL = ""
-export const OXFORD_DICTIONARY_APP_ID = ""
-export const OXFORD_DICTIONARY_APP_KEY = ""
+export const OXFORD_DICTIONARY_API_BASE_URL =
+  "https://od-api.oxforddictionaries.com/api/v2"
+export const OXFORD_DICTIONARY_APP_ID = "APP ID"
+export const OXFORD_DICTIONARY_APP_KEY = "APP KEY"
